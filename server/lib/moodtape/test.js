18fbd8ea8f3595321a8443f00c54629fa795788b
nickmazurenko <surprisedsunrise@gmail.com>
2022-08-16 19:26:52 +0300

    test update

--- server/lib/moodtape/test.js
+++ server/lib/moodtape/test.js
@@ -1,28 +1,42 @@
 import SpotifyWebApi from '../spotify-web-api-node/src/spotify-web-api.js';
 
 const credentials = {
-  clientId: '2227cc901288429abd075d6727d534ee',
-  clientSecret: '600914135c794a41a78ac5a2aed18163',
-  accessToken: 'BQCMZaisySGYUrj8mthNwUzbQnHHdGPLVMPJ0OkGxf9gFCr-LpQW_jN0B7ZYd4FZusrBkdmNSv0t5zwhgrYx_lAkUybWkmmPIwKoVXQ5IvqXX7MrUX4'
+  clientId: '7891eb892a224f01886f1d424be6c643',
+  clientSecret: 'c06f88450bc74137915ba8ddfe57254b',
+  accessToken:
+    'accessToken',
 };
 
 const spotifyApi = new SpotifyWebApi(credentials);
 
-// spotifyApi.getTrack('6vsEeepIYXoNMC7ROPfXoR').then((data) => {
-//   console.log(data);
-// })
+spotifyApi.getPlaylistTracks('37i9dQZF1DZ06evO4r8o3S').then((response) => {
+  const tracks = response.body.items;
+  const tracksId = tracks.map((data) => data.track.id);
 
-// spotifyApi.getAudioFeaturesForTrack('6vsEeepIYXoNMC7ROPfXoR').then((data) => {
-//   console.log(data);
-// }, (error) => {
-//   console.log(error);
-// });
+  spotifyApi.getAudioFeaturesForTracks(tracksId).then((data) => {
+    const features = data.body.audio_features;
+    const valence = features.reduce((sum, properties) => {
+      console.table({
+        energy: properties.energy,
+        danceability: properties.danceability,
+        loudness: properties.loudness,
+        acousticness: properties.acousticness,
+        liveness: properties.liveness,
+        valence: properties.valence,
+        tempo: properties.tempo,
+        id: properties.id
+      });
+      return sum + properties.valence;
+    }, 0);
 
-spotifyApi.getMyTopTracks({limit: 10})
-  .then(function(data) {
-    let topTracks = data.body.items;
-    console.log(topTracks);
-  }, function(err) {
-    console.log('Something went wrong!', err);
+    const mood = valence / features.length < 0.3 ? 'sad' : 'happy';
+
+    console.table({valence: valence / features.length, mood});
   });
+});
 
+const analyzeTrack = (trackId) => {
+  spotifyApi.getAudioFeaturesForTrack(trackId).then((data) => {
+    console.log(data);
+  });
+};
